e8b8ade63da24e3b0d4efc810459b437eb4460c2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2025-07-30 11:10:18 +0300

    Refactor

--- framework/src/Volo.Abp.AI/Volo/Abp/AI/AbpAIModule.cs
+++ framework/src/Volo.Abp.AI/Volo/Abp/AI/AbpAIModule.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
-using Volo.Abp.AI.Configuration;
 using Volo.Abp.AI.Options;
 using Volo.Abp.Modularity;
 
@@ -20,22 +17,13 @@ public class AbpAIModule : AbpModule
         var services = context.Services;
         var configuration = services.GetConfiguration();
 
-        // services.AddKeyedTransient<AbpChatClient>(
-        //     (string key, IServiceProvider sp) => 
-        //     new AbpChatClient(
-        //         ...
-        //     )
-        // );
-
-        // services.AddKeyedTransient<IChatClient, AbpChatClient>(
-        //     (object key, IServiceProvider sp) => (IChatClient)sp.GetRequiredKeyedService<AbpChatClient>(key?.ToString())
-        // );
-
         services.AddTransient(typeof(IChatClient<>), typeof(AbpChatClient<>));
 
-        Configure((ChatClientProviderOptions options) =>
+        Configure<ChatClientProviderOptions>(options =>
         {
-            options.ChatClients = configuration.GetSection("ChatClients").Get<Dictionary<string, ChatClientOptions>>() ?? new();
+            options.ChatClients = configuration
+                .GetSection("ChatClients")
+                .Get<Dictionary<string, ChatClientOptions>>() ?? new();
         });
 
         return Task.CompletedTask;
