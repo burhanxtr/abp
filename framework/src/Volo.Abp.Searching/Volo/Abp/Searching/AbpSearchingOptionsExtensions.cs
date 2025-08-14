03f874b18e627900262dbd2dbba1f777c7483f42
EngincanV <enginveske@gmail.com>
2025-08-14 16:05:54 +0300

    Update AbpSearchingOptionsExtensions.cs

--- framework/src/Volo.Abp.Searching/Volo/Abp/Searching/AbpSearchingOptionsExtensions.cs
+++ framework/src/Volo.Abp.Searching/Volo/Abp/Searching/AbpSearchingOptionsExtensions.cs
@@ -1,16 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 namespace Volo.Abp.Searching;
 
 public static class AbpSearchingOptionsExtensions
 {
-    public static void AddEntity<TEntity>(this AbpSearchingOptions options, Action<EntitySearchConfiguration> configure)
+    public static AbpSearchingOptions AddEntity<TEntity>(this AbpSearchingOptions options, Action<EntitySearchConfiguration> configure)
         where TEntity : class, ISearchableEntity
     {
         var entitySearchConfiguration = new EntitySearchConfiguration(typeof(TEntity));
         
         configure(entitySearchConfiguration);
         
-        options.Entities.Add(typeof(TEntity).FullName, entitySearchConfiguration);
+        options.Entities.Add(typeof(TEntity).FullName!, entitySearchConfiguration);
+
+        return options;
+    }
+
+    public static AbpSearchingOptions AddEntity<TEntity>(
+        this AbpSearchingOptions options,
+        params Func<TEntity, string>[] propertySelectors)
+        where TEntity : class, ISearchableEntity
+    {
+        options.AddEntity<TEntity>(opts =>
+        {
+            foreach (var propertySelector in propertySelectors)
+            {
+                opts.SearchableProperties.Add(new SearchableProperty(propertySelector(default(TEntity)!)));
+            }
+        });
+
+        return options;
     }
 }
